ca5a1286b33a784c34fec9f8dbf6c5f2eef7e93d
Mariia Mikhalchenko <m.mikhalchenko.de@gmail.com>
2024-03-01 16:34:25 +0100

    init

---
+++ src/components/appBanner/AppBanner.js
@@ -0,0 +1,18 @@
+import './appBanner.scss';
+import avengers from '../../resources/img/Avengers.png';
+import avengers_logo from '../../resources/img/Avengers_logo.png';
+
+const AppBanner = () => {
+  return (
+    <div className="app__banner">
+      <img src={avengers} alt="Avengers" />
+      <div className="app__banner-text">
+        New comics every week!<br/>
+        Stay tuned!
+      </div>
+      <img src={avengers_logo} alt="Avengers logo" />
+    </div>
+  )
+}
+
+export default AppBanner;
