b43fae52d62cb252490d2af38526f496dc8c1f27
Mariia Mikhalchenko <m.mikhalchenko.de@gmail.com>
2024-03-05 22:41:22 +0100

    feat: add spinner and error components

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,20 @@
+import img from './error.gif';
+
+const ErrorMessage = ({ error }) => {
+  return (
+    // <img src={process.env.PUBLIC_URL + '/error.gif'}/>
+    <img
+      style={{
+        display: 'block',
+        width: '250px',
+        height: '250px',
+        objectFit: 'contain',
+        margin: '0 auto',
+      }}
+      src={img}
+      alt="Error"
+    />
+  );
+};
+
+export default ErrorMessage;
