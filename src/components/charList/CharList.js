5eb566155af28e62f314980a78955d725873b4b1
Mariia Mikhalchenko <m.mikhalchenko.de@gmail.com>
2024-03-28 21:44:16 +0100

    feat: add focus on character

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -14,7 +14,7 @@ class CharList extends Component {
     error: false,
     newItemLoading: false,
     offset: 210,
-    charEnded: false,
+    charEnded: false
   };
 
   marvelService = new MarvelService();
@@ -74,8 +74,20 @@ class CharList extends Component {
     });
   };
 
+  itemRefs = [];
+
+  setRef = (ref) => {
+    this.itemRefs.push(ref);
+  }
+
+  focusOnItem = (id) => {
+    this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+    this.itemRefs[id].classList.add('char__item_selected');
+    this.itemRefs[id].focus();
+  };
+
   renderItems(arr) {
-    const items = arr.map((item) => {
+    const items = arr.map((item, i) => {
       let imgStyle = { objectFit: 'cover' };
       if (
         item.thumbnail ===
@@ -87,8 +99,17 @@ class CharList extends Component {
       return (
         <li
           className="char__item"
+          tabIndex={0}
+          ref={this.setRef}
           key={item.id}
-          onClick={() => this.props.onCharSelected(item.id)}
+          onClick={() => {
+            this.props.onCharSelected(item.id)
+            this.focusOnItem(i)}}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              this.props.onCharSelected(item.id);
+              this.focusOnItem(i);
+          }}}
         >
           <img src={item.thumbnail} alt={item.name} style={imgStyle} />
           <div className="char__name">{item.name}</div>
