c6ffe55aecdcd7bcb5c0f22c865ab3efdb082ff9
Mariia Mikhalchenko <m.mikhalchenko.de@gmail.com>
2024-06-07 16:42:33 +0200

    feat: add links to comic

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import {MainPage, ComicsPage} from '../pages';
+import { MainPage, ComicsPage, Page404, SingleComicPage } from '../pages';
 import AppHeader from '../appHeader/AppHeader';
 
 const App = () => {
@@ -10,8 +10,10 @@ const App = () => {
         <AppHeader />
         <main>
           <Routes>
-            <Route path="/" element={<MainPage/>} />
-            <Route path="/comics" element={<ComicsPage/>} />
+            <Route path="/" element={<MainPage />} />
+            <Route path="/comics" element={<ComicsPage />} />
+            <Route path="/comics/:comicId" element={<SingleComicPage />} />
+            <Route path="*" element={<Page404 />} />
           </Routes>
         </main>
       </div>
